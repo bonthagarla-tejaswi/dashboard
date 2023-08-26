ea443d07c71d79aa7cd1e74a39ca70a5b430fe79
anilreddykota <22b91a5469@srkrec.ac.in>
2023-08-26 15:58:51 +0530

    pushed one

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 let db; 
 async function connectToDB(cb){
-    const url = "mongodb+srv://anilreddygree:6302423327*Aa@cluster0.mk1a8vu.mongodb.net/?retryWrites=true&w=majority";
+    const url = "mongodb+srv://kotaanilreddy93:Anil*1234@cluster0.j5owezv.mongodb.net/?retryWrites=true&w=majority";
     const client = new MongoClient(url);
     await client.connect();
     db = client.db("dashboarddata");
