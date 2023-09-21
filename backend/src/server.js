cf3bd0f65f1ef9ad7a3f4dd885ab8e48f1749b66
anilreddykota <22b91a5469@srkrec.ac.in>
2023-09-21 13:13:13 +0530

    edhi aa bomma

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import { connectToDB, db } from './db.js';
 const app=express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
+
 //check server//
 app.post('/',(req,res)=>{
     res.send("server is running")
